d4f3c42ef3f8c04c0215bd0cd130aeb1ece79411
juliaeis <jeis@uni-bremen.de>
2019-07-31 14:30:03 +0200

    measure for different t

--- paper/alps_experiments.py
+++ paper/alps_experiments.py
@@ -25,7 +25,34 @@ mpl.rcParams['axes.labelweight'] = 'medium'
 mpl.rcParams['legend.fontsize'] = 20 #30
 mpl.rcParams['lines.linewidth'] = 3
 
+from scipy.stats import pearsonr
 
+def calculate_pvalues(df):
+    df = df._get_numeric_data()
+    dfcols = pd.DataFrame(columns=df.columns)
+    pvalues = dfcols.transpose().join(dfcols, how='outer')
+    for r in df.columns:
+        for c in df.columns:
+            if c == r:
+                df_corr = df[[r]].dropna()
+            else:
+                df_corr = df[[r,c]].dropna()
+            pvalues[r][c] = pearsonr(df_corr[r], df_corr[c])[1]
+    return pvalues
+
+def corr_pvalue(df):
+    numeric_df = df.dropna()._get_numeric_data()
+    cols = numeric_df.columns
+    mat = numeric_df.values
+
+    arr = np.zeros((len(cols),len(cols)), dtype=object)
+
+    for xi, x in enumerate(mat.T):
+        for yi, y in enumerate(mat.T[xi:]):
+            arr[xi, yi+xi] = map(lambda _: round(_,4), pearsonr(x,y))
+            arr[yi+xi, xi] = arr[xi, yi+xi]
+
+    return pd.DataFrame(arr, index=cols, columns=cols)
 
 def find_median(df):
 
@@ -57,7 +84,7 @@ def read_results(gdirs):
     return model_df
 
 
-def plot_ratio_volume(df,ex_mod,gdir, ratio2):
+def plot_ratio_volume(df,acc_df,perc_df,ex_mod,gdir, ratio, t):
 
     fig, ax = plt.subplots(1,1, figsize=(10,7))
     norm = mpl.colors.LogNorm(vmin=0.01 / 125, vmax=10)
@@ -65,78 +92,83 @@ def plot_ratio_volume(df,ex_mod,gdir, ratio2):
 
     # df = df.sort_values('objective', ascending=False)
     df = df.sort_values('fitness', ascending=False)
+    acc_min = acc_df.loc[acc_df.volume.idxmin(),'model'].volume_km3_ts()
+    acc_max = acc_df.loc[acc_df.volume.idxmax(),'model'].volume_km3_ts()
+    perc_min = perc_df.loc[perc_df.volume.idxmin(), 'model'].volume_km3_ts()
+    perc_max = perc_df.loc[perc_df.volume.idxmax(), 'model'].volume_km3_ts()
 
-    for i, model in df['model'].iteritems():
-        try:
-
-            model = deepcopy(model)
-            color = cmap(norm(df.loc[i, 'fitness']))
-            model.volume_km3_ts().plot(ax=ax, color=[color], label='')
-        except:
-            pass
+    ax.fill_between(acc_max.index, acc_max.values,acc_min.values, color='grey', alpha=0.3)
+    ax.fill_between(perc_max.index, perc_max.values, perc_min.values, color='C0', alpha=0.5)
     ex_mod.volume_km3_ts().plot(ax=ax, color='red', linestyle=':',
                                 linewidth=3,
                                 label='')
+    ax.axvline(x=t, color='k')
     plt.title(gdir.rgi_id)
-
-    plt.text(1950, 0.95 * df.volume.max(), 'ratio2: ' + str(ratio2.round(4)))
+    plt.text(1950, 0.95 * acc_max.max(), 'measure: ' + str(ratio.round(4)))
+    plt.text(1950, 0.9 * acc_max.max(), 't: ' + str(t))
 
     plt.ylabel('Volume (km$^3$)')
     plt.ylabel('Volume (km$^3$)')
     plt.xlabel('Time (years)')
     utils.mkdir(os.path.join(cfg.PATHS['plot_dir'],'ratio'))
     plt.savefig(os.path.join(cfg.PATHS['plot_dir'],'ratio',gdir.rgi_id+'.png'))
-    plt.show()
+    #plt.show()
 
 def read_result_parallel(gdir):
 
+    #try:
+    rp = gdir.get_filepath('model_run', filesuffix='_experiment')
+    ex_mod = FileModel(rp)
+
+    df = pd.read_pickle(os.path.join(gdir.dir, 'result1850.pkl'),
+                        compression='gzip')
+
+    df.fitness = df.fitness / 125
+    # replace all values smaller than 1e-4
+    df.fitness[df.fitness < 1e-4] = 1e-4
+    acc_df = df[df.fitness<1]
+    perc_df = acc_df[acc_df.fitness <= round(acc_df.fitness.quantile(0.05),4)]
+
+    v_acc = acc_df.model.apply(lambda x: x.volume_km3_ts())
+    v_perc = perc_df.model.apply(lambda x: x.volume_km3_ts())
+
     try:
-        rp = gdir.get_filepath('model_run', filesuffix='_experiment')
-        ex_mod = FileModel(rp)
-
-        df = pd.read_pickle(os.path.join(gdir.dir, 'result1850.pkl'),
-                            compression='gzip')
-        df.fitness = df.fitness / 125
-        # replace all values smaller than 1e-4
-        df.fitness[df.fitness < 1e-4] = 1e-4
-
-        '''
-        min = df.fitness.min()
-        max = df.fitness.max()
-        median = df.fitness.median()
-        q5 = df.fitness.quantile(0.05)
-        ratio = (0.6)*(1-(min / q5)) + (0.3)*(1-(q5 / median))+(0.1)*(1-(median/max))
-        '''
-        acc_df = df[df.fitness<1]
-        perc_df = acc_df[acc_df.fitness <= acc_df.fitness.quantile(0.05).round(4)]
-
-        r1 =  acc_df.volume.max() - acc_df.volume.min()
-        r2 =  perc_df.volume.max() - perc_df.volume.min()
-
-        ratio = 1-(r2/r1)
-
-        plot_ratio_volume(df,ex_mod,gdir, ratio)
-
-        inv_in = gdir.read_pickle('inversion_input')
-        slope = []
-        for i in range(len(inv_in)):
-            slope.extend(inv_in[-1]['slope_angle'])
-
-        ds = xr.open_dataset(gdir.get_filepath('model_diagnostics', filesuffix='_experiment'))
-        diag = ds.to_dataframe()
-
-        return pd.Series({'rgi':gdir.rgi_id, 'ratio':ratio,
-                          'length':ex_mod.length_m_ts()[2000],
-                          'area':ex_mod.area_km2_ts()[2000],
-                          'volume':ex_mod.volume_km3_ts()[2000],
-                          'slope_max':np.max(slope), 'slope_mean':np.mean(slope),
-                          'ela_2000':diag.ela_m[2000],
-                          'ela_change':diag.ela_m[1850]/diag.ela_m[2000]})
-
-        return pd.Series({'rgi':gdir.rgi_id})
-
+        ratio = (v_perc.max()-v_perc.min())/(v_acc.max()[1850]-v_acc.min()[1850])
+        t = int(ratio[ratio<0.01].index[0])
     except:
-        return pd.Series({'rgi':gdir.rgi_id})
+        t=2000
+
+    plot_ratio_volume(df,acc_df, perc_df,ex_mod,gdir, ratio.loc[1850], t)
+
+    inv_in = gdir.read_pickle('inversion_input')
+    slope = []
+    slope2 = []
+    slope3 = []
+    for i in range(len(inv_in)):
+        n = int(len(inv_in[-1]['slope_angle'])/3)
+
+        slope.extend(inv_in[i]['slope_angle'])
+        slope2.extend(inv_in[i]['slope_angle'][-n:])
+        slope3.extend(inv_in[i]['slope_angle'][-2*n:])
+
+
+    ds = xr.open_dataset(gdir.get_filepath('model_diagnostics', filesuffix='_experiment'))
+    diag = ds.to_dataframe()
+
+    return pd.Series({'rgi':gdir.rgi_id, 'ratio':ratio, 'time':t,
+                      'length':ex_mod.length_m_ts()[2000],
+                      'area':ex_mod.area_km2_ts()[2000],
+                      'volume':ex_mod.volume_km3_ts()[2000],
+                      'slope_max':np.max(slope),
+                      'slope_mean':np.mean(slope),
+                      'slope_median':np.median(slope),
+                      'slope_third': np.mean(slope2),
+                      'slope_2thrid': np.mean(slope3),
+                      'ela_2000':diag.ela_m[2000],
+                      'ela_change':diag.ela_m[1850]/diag.ela_m[2000]})
+
+    #except:
+    #    return pd.Series({'rgi':gdir.rgi_id})
 
 if __name__ == '__main__':
     cfg.initialize()
@@ -187,33 +219,28 @@ if __name__ == '__main__':
     # RGI file
     path = utils.get_rgi_region_file('11', version='61')
     rgidf = gpd.read_file(path)
-    #rgidf = rgidf[rgidf.RGIId.isin(['RGI60-11.00897','RGI60-11.00779', 'RGI60-11.00029', 'RGI60-11.00036', 'RGI60-11.00001','RGI60-11.00026','RGI60-11.00062'])]
-    rgidf = rgidf[rgidf.RGIId.isin(['RGI60-11.00074'])]
+    rgidf = rgidf[rgidf.RGIId.isin(['RGI60-11.00897','RGI60-11.00779', 'RGI60-11.00029', 'RGI60-11.00036', 'RGI60-11.00001','RGI60-11.00026','RGI60-11.00062'])]
+    #rgidf = rgidf[rgidf.RGIId.isin(['RGI60-11.00074'])]
     # sort for efficient using
     rgidf = rgidf.sort_values('Area', ascending=False)
-
     gdirs = workflow.init_glacier_regions(rgidf)
-    df = read_results(gdirs).dropna()
-    #print(df)
-    #df.to_pickle(os.path.join(cfg.PATHS['working_dir'],'ratio.pkl'),compression='gzip')
 
+    df = read_results(gdirs).dropna()
+    print(df)
+    #df.to_pickle(os.path.join(cfg.PATHS['working_dir'],'ratio.pkl'),compression='gzip')
+    '''
 
     df = pd.read_pickle(os.path.join(cfg.PATHS['working_dir'],'ratio.pkl'),compression='gzip')
-
-    '''
-    df = df.drop('rgi_id',axis=1).dropna()
-
-    #for i, col in enumerate(df.drop(['rgi','ratio'],axis=1)):
+        #for i, col in enumerate(df.drop(['rgi','ratio'],axis=1)):
         #fig, ax = plt.subplots(1, 1)
         #df.plot.scatter(x=col,y='ratio',ax=ax)
     #plt.show()
-    df = df[['rgi','ratio','length','area','volume','ela_2000','ela_change', 'slope_max','slope_mean']]
+    df = df[['rgi','ratio','length','area','volume','ela_2000','ela_change', 'slope_max','slope_mean']].dropna()
 
     fig,ax = plt.subplots(1,1,figsize=(10,15))
-    print(df.corr()['ratio'])
+    p = calculate_pvalues(df)
     matrix = ax.matshow(df.drop('rgi',axis=1).corr(),vmin=-1,vmax=1, cmap='RdBu_r')
-    print(matrix)
-    marks = ['measure', 'Lenght', 'Area','Volume', r'ELA$_{2000}$', r'ELA$_{1850}$/ELA$_{2000}$',
+    marks = ['measure', 'Length', 'Area','Volume', r'ELA$_{2000}$', r'ELA$_{1850}$/ELA$_{2000}$',
               r'Slope$_{max}$', r'Slope$_{mean}$']
     tick_marks = [i for i in range(len(df.drop('rgi',axis=1).columns))]
     plt.xticks(tick_marks, marks, rotation='vertical')
@@ -222,29 +249,35 @@ if __name__ == '__main__':
     plt.tight_layout()
     plt.show()
 
+    from sklearn.linear_model import LinearRegression
+
+    X = df.ratio.values.reshape(-1, 1)
+    Y = df.slope_mean.values.reshape(-1, 1)
+    linear_regressor = LinearRegression()  # create object for the class
+    linear_regressor.fit(X, Y)  # perform linear regression
+    Y_pred = linear_regressor.predict(X)
+
+
     fig,ax = plt.subplots(1,1)
-    df.plot.scatter(x='ratio2', y='area',color='C1',ax=ax,label='ratio')
+    df.plot.scatter(x='ratio', y='slope_mean',color='C1',ax=ax,label='ratio')
+    ax.plot(X, Y_pred, color='C1')
     plt.xlabel('reconstructability')
     plt.ylabel('Glacier Area (km²)')
 
-    fig, ax = plt.subplots(1, 1)
-    # df.plot.scatter(x='ratio1',y='area',color='C0',ax=ax,label='ratio1')
-    df.plot.scatter(x='ratio3', y='area', color='C2', ax=ax, label='ratio3')
-
-    plt.xlabel('reconstructability')
-    plt.ylabel('Glacier Area (km²)')
-    #plt.show()
+    plt.xlabel('reconstructability measure')
+    plt.ylabel('mean slope')
+    plt.show()
 
 
 
-        print(gdir.rgi_id)
-        print(len(df[df.fitness<1e-4]))
-        print(len(df[(df.fitness > 1e-4) & (df.fitness< 1e-3)]))
-        print(len(df[(df.fitness > 1e-3) & (df.fitness < 1e-2)]))
-        print(len(df[(df.fitness > 1e-2) & (df.fitness < 1e-1)]))
-        print(len(df[(df.fitness > 1e-1) & (df.fitness < 1e-0)]))
-        print(len(df[(df.fitness > 1e0) & (df.fitness < 1e1)]))
-        print(len(df[df.fitness > 1e1]))
+    print(gdir.rgi_id)
+    print(len(df[df.fitness<1e-4]))
+    print(len(df[(df.fitness > 1e-4) & (df.fitness< 1e-3)]))
+    print(len(df[(df.fitness > 1e-3) & (df.fitness < 1e-2)]))
+    print(len(df[(df.fitness > 1e-2) & (df.fitness < 1e-1)]))
+    print(len(df[(df.fitness > 1e-1) & (df.fitness < 1e-0)]))
+    print(len(df[(df.fitness > 1e0) & (df.fitness < 1e1)]))
+    print(len(df[df.fitness > 1e1]))
     plt.show()
 
     df = pd.read_pickle(os.path.join(cfg.PATHS['working_dir'], 'ratio.pkl'),
