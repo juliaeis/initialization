83350b4dee4dbae33270a3f86caba990c7fabadc
juliaeis <jeis@uni-bremen.de>
2019-10-21 11:52:25 +0200

    correct path from outdir

--- global_run/global_test.py
+++ global_run/global_test.py
@@ -2,6 +2,7 @@
 import sys
 sys.path.append('../')
 from initialization.core import *
+from paper.plots_paper import *
 from shutil import copyfile
 
 
@@ -22,6 +23,7 @@ if __name__ == '__main__':
         cfg.PATHS['working_dir'] = WORKING_DIR
         OUT_DIR = os.environ.get("OUTDIR")
         REGION = str(os.environ.get('REGION')).zfill(2)
+        ID =  os.environ.get("I")
     else:
         WORKING_DIR = '/home/juliaeis/Dokumente/OGGM/work_dir/reconstruction/global/'
         OUT_DIR = WORKING_DIR
@@ -56,25 +58,21 @@ if __name__ == '__main__':
     # RGI file
     path = utils.get_rgi_region_file(REGION, version='61')
     rgidf = gpd.read_file(path)
-    rgidf = rgidf.sort_values('Area', ascending=True)
-    print(rgidf[rgidf.Area > 5].Area)
-
+    rgidf = rgidf.sort_values('Area', ascending=False)
+    rgidf = rgidf[rgidf.RGIId == 'RGI60-05.07854']
     #select HEF
-    gdirs = workflow.init_glacier_regions(rgidf[1:2])
+    gdirs = workflow.init_glacier_regions(rgidf)
+
 
     t_0 = 1917
-
     epsilon = 125
-    preprocessing(gdirs)
+    #preprocessing(gdirs)
     #advanced_experiments(gdirs, [0], 1917, REGION)
 
-   for gdir in gdirs:
-        # split command works different on Cluster and localy
-        if ON_CLUSTER:
-            dir = os.path.join(OUT_DIR,'global', gdir.dir.split('/global/')[-1])
-        else:
-            dir = os.path.join(OUT_DIR, gdir.dir.split('/global/')[-1])
+    for gdir in gdirs:
 
+        dir = os.path.join(OUT_DIR, gdir.dir.split('/global/')[-1])
+        '''
         ex = [f for f in os.listdir(dir) if f.startswith('model_run_ad')]
         t_e = gdir.rgi_date
         if len(ex)==1 :
@@ -85,12 +83,21 @@ if __name__ == '__main__':
                 copyfile(src, dst)
             else:
                 dst = os.path.join(gdir.dir,ex[0])
+        '''
+
+        try:
+            print(os.listdir(gdir.dir))
+            shutil.copytree(dir, gdir.dir)
+            print(os.listdir(gdir.dir))
+            '''
 
             ex_mod = FileModel(dst)
             bias = float(ex[0].split('_')[-1].split('.nc')[0])
-            print(ex_mod.area_km2_ts()[t_e])
-        '''
-        df = find_possible_glaciers(gdir, t_0, t_e, 200, ex_mod, bias)
 
-        '''
+            df = find_possible_glaciers(gdir, t_0, t_e, 100, ex_mod, bias, delete=True)
+            save = pickle.load(open(os.path.join(gdir.dir, 'initialization_output.pkl'),'rb'))
+            print(save)
+            '''
+        except:
+            pass
 
